052717fb0b8a19e6ada2d28e46415847a1ca6818
Misaka Mikoto <easy66@live.com>
2023-10-15 00:53:14 +0800

    * 撤回消息逻辑修改

--- haruka_bot/plugins/recall_msg.py
+++ haruka_bot/plugins/recall_msg.py
@@ -1,14 +1,14 @@
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
 from nonebot.plugin import on_keyword
 from nonebot.matcher import Matcher
-from ..utils import permission_check, to_me
-from nonebot.log import logger
+from nonebot.permission import SUPERUSER
 
-on_recall = on_keyword({"撤回消息"}, priority=5, block=True)
-on_recall.__doc__ = "撤回消息"
+on_recall = on_keyword({"撤回"}, permission=SUPERUSER)
 
 @on_recall.handle()
 async def recall_handle(matcher:Matcher, bot: Bot, event: GroupMessageEvent):
     if event.reply:
-        await permission_check(matcher, bot, event)
+        text = event.get_plaintext().strip()
+        if text != '撤回':
+            return
         await bot.delete_msg(message_id=event.reply.message_id)
