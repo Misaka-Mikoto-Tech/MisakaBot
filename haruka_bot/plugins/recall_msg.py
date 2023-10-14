307b2af25a1e7b3d74f09597331b41c537e3786a
Misaka Mikoto <easy66@live.com>
2023-10-14 23:02:51 +0800

    * 修改撤回功能的权限检查逻辑

--- haruka_bot/plugins/recall_msg.py
+++ haruka_bot/plugins/recall_msg.py
@@ -1,13 +1,13 @@
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
 from nonebot.plugin import on_command
+from nonebot.matcher import Matcher
 from ..utils import permission_check
 
 on_recall = on_command("撤回", aliases={"recall", "delete"}, priority=5, block=True)
 on_recall.__doc__ = "撤回消息"
 
-on_recall.handle()(permission_check)
-
 @on_recall.handle()
-async def recall_handle(bot: Bot, event: GroupMessageEvent):
+async def recall_handle(matcher:Matcher, bot: Bot, event: GroupMessageEvent):
     if event.reply:
+        await permission_check(matcher, bot, event)
         await bot.delete_msg(message_id=event.reply.message_id)
--- haruka_bot/utils/__init__.py
+++ haruka_bot/utils/__init__.py
@@ -97,7 +97,7 @@ async def permission_check(
     bot: Bot,
     event: Union[GroupMessageEvent, GroupMessageSentEvent, PrivateMessageEvent, GuildMessageEvent]
 ):
-    """检查推送相关操作是否有权限"""
+    """检查推送相关操作是否有权限，无权限时抛出 FinishedException 异常"""
     bot_id = int(bot.self_id)
     async def check_exclusive_bot():
         if (bot_id in config.exclusive_bots) and (event.sender.user_id != bot_id):
