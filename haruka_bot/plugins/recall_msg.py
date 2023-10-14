e64e5c57a040645503ef2f4e426d84a326122eae
Misaka Mikoto <easy66@live.com>
2023-10-14 14:53:24 +0800

    * 给撤回消息功能添加权限控制

--- haruka_bot/plugins/recall_msg.py
+++ haruka_bot/plugins/recall_msg.py
@@ -1,9 +1,12 @@
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
 from nonebot.plugin import on_command
+from ..utils import permission_check
 
 on_recall = on_command("撤回", aliases={"recall", "delete"}, priority=5, block=True)
 on_recall.__doc__ = "撤回消息"
-    
+
+on_recall.handle()(permission_check)
+
 @on_recall.handle()
 async def recall_handle(bot: Bot, event: GroupMessageEvent):
     if event.reply:
