d5e83969da1b7cc2643ac0a67fc2ce10b9eaf9c3
Misaka-Mikoto <easy66@live.com>
2023-10-24 17:51:30 +0800

    * 增加 debug time 指令

--- haruka_bot/plugins/debug.py
+++ haruka_bot/plugins/debug.py
@@ -1,3 +1,4 @@
+import time
 from typing import List
 from nonebot.matcher import matchers
 from nonebot.adapters.onebot.v11 import Bot, MessageSegment
@@ -14,10 +15,15 @@ from .. import config
 debug = on_command("debug", permission=SUPERUSER, priority=2, block=True)
 
 @debug.handle()
-async def _(event: GroupMessageEvent, bot:Bot, matcher: Matcher, arg: Message = CommandArg()):
+async def _(event: GroupMessageEvent, bot:Bot, matcher: Matcher, argMsg: Message = CommandArg()):
     """输出回复的原始消息内容"""
+    arg = argMsg.extract_plain_text().strip()
     if event.reply:
-        if arg.extract_plain_text().strip() == 'echo':
+        if arg == 'echo':
             await matcher.finish(event.reply.message)
         else:
             await matcher.finish('{' + str(event.reply) +'}')
+    else:
+        if arg == 'time':
+            now = time.localtime(time.time())
+            await matcher.finish(time.strftime("%Y-%m-%d %H:%M:%S", now))
