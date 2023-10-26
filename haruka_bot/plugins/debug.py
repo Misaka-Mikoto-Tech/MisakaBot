addee5b97725ca18deb140fcc0c9b911b96972a5
Misaka-Mikoto <easy66@live.com>
2023-10-26 18:17:35 +0800

    * debug  指令允许管理员执行

--- haruka_bot/plugins/debug.py
+++ haruka_bot/plugins/debug.py
@@ -5,6 +5,7 @@ from nonebot.matcher import matchers
 from nonebot.adapters.onebot.v11 import Bot, MessageSegment
 from nonebot.adapters.onebot.v11.event import GroupMessageEvent, Reply
 from nonebot.permission import SUPERUSER
+from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
 from nonebot.adapters.onebot.v11.message import Message
 from nonebot.params import CommandArg
 from nonebot.internal.matcher import Matcher
@@ -13,7 +14,7 @@ from ..utils import on_command, to_me, text_to_img
 from ..version import __version__
 from .. import config
 
-debug = on_command("debug", permission=SUPERUSER, priority=2, block=True)
+debug = on_command("debug", permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER, priority=2, block=True)
 
 @debug.handle()
 async def _(event: GroupMessageEvent, bot:Bot, matcher: Matcher, argMsg: Message = CommandArg()):
