d92a1a36810e8d2bca3c5fad45dcd0d7697496f3
Misaka-Mikoto <easy66@live.com>
2023-11-07 10:37:36 +0800

    * B站关注取关改为使用用户名

--- haruka_bot/plugins/sub/add_sub.py
+++ haruka_bot/plugins/sub/add_sub.py
@@ -1,7 +1,9 @@
 from bilireq.exceptions import ResponseCodeError
 from bilireq.user import get_user_info
 from nonebot.adapters.onebot.v11.event import MessageEvent
-from nonebot.params import ArgPlainText
+from nonebot.adapters.onebot.v11 import Bot, MessageSegment, Message
+from nonebot.params import ArgPlainText, CommandArg
+from nonebot.internal.matcher import Matcher, current_event
 from nonebot_plugin_guild_patch import GuildMessageEvent
 
 from ...database import DB as db
@@ -14,36 +16,32 @@ from ...utils import (
     to_me,
     uid_check,
 )
+from ...utils.uid_extract import uid_extract
 from ...bili_auth import bili_auth
 
 add_sub = on_command("关注", aliases={"添加主播"}, rule=to_me(), priority=5, block=True)
-add_sub.__doc__ = """关注 UID"""
+add_sub.__doc__ = """关注 用户名"""
 
 add_sub.handle()(permission_check)
 
-add_sub.handle()(handle_uid)
-
-add_sub.got("uid", prompt="请输入要关注的UID")(uid_check)
-
-
 @add_sub.handle()
-async def _(event: MessageEvent, uid: str = ArgPlainText("uid")):
-    """根据 UID 订阅 UP 主"""
-    user = await db.get_user(uid=uid)
-    name = user and user.name
-    if not name:
-        try:
-            name = (await get_user_info(uid, proxies=PROXIES, auth=bili_auth.auth))["name"]
-        except ResponseCodeError as e:
-            if e.code == -400 or e.code == -404:
-                await add_sub.finish("UID不存在，注意UID不是房间号")
-            elif e.code == -412:
-                await add_sub.finish("操作过于频繁IP暂时被风控，请半小时后再尝试")
-            else:
-                await add_sub.finish(
-                    f"未知错误，请联系开发者反馈，错误内容：\n\
-                                    {str(e)}"
-                )
+async def _(
+    matcher: Matcher, event: MessageEvent, bot:Bot, arg_msg: Message = CommandArg()
+):
+    if arg_msg.extract_plain_text().strip():
+        matcher.set_arg("user_name", arg_msg)
+
+@add_sub.got("user_name", "请发送UP名称")
+async def _(event: MessageEvent, user_name: str = ArgPlainText("user_name")):
+    """根据 用户名 订阅 UP 主"""
+    user_name = user_name.strip()
+    if not (uid := await uid_extract(user_name)):
+        return await add_sub.send(MessageSegment.at(event.user_id) + " 未找到该 UP，请输入正确的UP 名、UP UID或 UP 首页链接")
+    
+    if isinstance(uid, list):
+        return await add_sub.send(MessageSegment.at(event.user_id) + f" 未找到{user_name}, 你是否想要找:\n" + '\n'.join([item['uname'] for item in uid[:10] ]))
+    elif int(uid) == 0:
+        return await add_sub.send(MessageSegment.at(event.user_id) + " UP 主不存在")
 
     if isinstance(event, GuildMessageEvent):
         await db.add_guild(
@@ -54,7 +52,7 @@ async def _(event: MessageEvent, uid: str = ArgPlainText("uid")):
         type=event.message_type,
         type_id=await get_type_id(event),
         bot_id=event.self_id,
-        name=name,
+        name=user_name,
         # TODO 自定义默认开关
         live=True,
         dynamic=True,
@@ -62,5 +60,5 @@ async def _(event: MessageEvent, uid: str = ArgPlainText("uid")):
         live_tips=''
     )
     if result:
-        await add_sub.finish(f"已关注 {name}（{uid}）")
-    await add_sub.finish(f"{name}（{uid}）已经关注了")
+        await add_sub.finish(f"已关注 {user_name}（{uid}）")
+    await add_sub.finish(f"{user_name}（{uid}）已经关注了")
--- haruka_bot/plugins/sub/delete_sub.py
+++ haruka_bot/plugins/sub/delete_sub.py
@@ -1,6 +1,8 @@
 from nonebot.adapters.onebot.v11 import Bot
 from nonebot.adapters.onebot.v11.event import MessageEvent
-from nonebot.params import ArgPlainText
+from nonebot.params import ArgPlainText, CommandArg
+from nonebot.internal.matcher import Matcher, current_event
+from nonebot.adapters.onebot.v11 import Bot, MessageSegment, Message
 
 from ...database import DB as db
 from ...utils import (
@@ -13,20 +15,23 @@ from ...utils import (
 )
 
 delete_sub = on_command("取关", aliases={"删除主播"}, rule=to_me(), priority=5, block=True)
-delete_sub.__doc__ = """取关 UID"""
+delete_sub.__doc__ = """取关 用户名"""
 
 delete_sub.handle()(permission_check)
 
-delete_sub.handle()(handle_uid)
-
-delete_sub.got("uid", prompt="请输入要取关的UID")(uid_check)
-
-
 @delete_sub.handle()
-async def _(event: MessageEvent, bot: Bot, uid: str = ArgPlainText("uid")):
-    """根据 UID 删除 UP 主订阅"""
-    name = getattr(await db.get_user(uid=uid), "name", None)
-    if name:
+async def _(
+    matcher: Matcher, event: MessageEvent, bot:Bot, arg_msg: Message = CommandArg()
+):
+    if arg_msg.extract_plain_text().strip():
+        matcher.set_arg("user_name", arg_msg)
+
+@delete_sub.got("user_name", "请发送UP名称")
+async def _(event: MessageEvent, bot: Bot, user_name: str = ArgPlainText("user_name")):
+    """根据 用户名 删除 UP 主订阅"""
+    user_name = user_name.strip()
+    uid = getattr(await db.get_user(name=user_name), "uid", None)
+    if uid:
         result = await db.delete_sub(
             uid=uid, type=event.message_type, type_id=await get_type_id(event), bot_id=bot.self_id
         )
@@ -34,5 +39,5 @@ async def _(event: MessageEvent, bot: Bot, uid: str = ArgPlainText("uid")):
         result = False
 
     if result:
-        await delete_sub.finish(f"已取关 {name}（{uid}）")
+        await delete_sub.finish(f"已取关 {user_name}（{uid}）")
     await delete_sub.finish(f"UID（{uid}）未关注")
