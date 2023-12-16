1470a40c7f00c9ae2da35883d865e6f497c8e0dc
Misaka Mikoto <easy66@live.com>
2023-12-16 22:49:30 +0800

    * 增加群号白名单函数

--- haruka_bot/plugins/only_me_mode.py
+++ haruka_bot/plugins/only_me_mode.py
@@ -3,14 +3,14 @@ from nonebot.matcher import Matcher
 from nonebot.permission import SUPERUSER
 from nonebot.exception import IgnoredException
 # from nonebot.adapters import Bot, Event
-from nonebot.adapters.onebot.v11 import Bot, MessageEvent
-from nonebot.message import run_preprocessor
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, GroupMessageEvent
+from nonebot.message import run_preprocessor, event_preprocessor
 from nonebot.log import logger
 
 only_me_mode: bool = False
 
-only_me_on = on_command("只听我的", aliases={"凛雅,只听我的", "凛雅，只听我的"}, permission=SUPERUSER, priority=5, block=True)
-only_me_off = on_command("听大家的", aliases={"凛雅,听大家的", "凛雅，听大家的"}, permission=SUPERUSER, priority=5, block=True)
+only_me_on = on_command("只听我的", aliases={"凛雅,只听我的", "凛雅，只听我的"}, permission=SUPERUSER, priority=1, block=True)
+only_me_off = on_command("听大家的", aliases={"凛雅,听大家的", "凛雅，听大家的"}, permission=SUPERUSER, priority=1, block=True)
 
 @only_me_on.handle()
 @only_me_off.handle()
@@ -34,3 +34,9 @@ async def check_only_me_mode(bot: Bot, event: MessageEvent, matcher: Matcher):
         # await matcher.finish(f'达咩')
         raise IgnoredException("独占模式，取消执行Matcher")
     
+@event_preprocessor
+async def on_event_pre_processor(bot: Bot, event: MessageEvent, matcher: Matcher):
+    """事件过滤函数"""
+    # if isinstance(event, GroupMessageEvent):
+    #     if not event.group_id in [123456, 789]: # 白名单群号列表，可以填入配置文件
+    #         raise IgnoredException("群号不在白名单中，取消派发")
