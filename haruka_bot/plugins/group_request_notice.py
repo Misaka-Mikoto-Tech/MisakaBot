bf726bf430e4ae488998f4e932de38d01608b974
Misaka Mikoto <easy66@live.com>
2023-12-30 01:35:17 +0800

    * 加群申请消息改为使用 get_stranger_info 获取 nickname (ob标准里GroupRequestEvent不包含nickname字段)

--- haruka_bot/plugins/group_request_notice.py
+++ haruka_bot/plugins/group_request_notice.py
@@ -11,8 +11,12 @@ group_request = on_request(priority=5)
 @group_request.handle()
 async def _(event: GroupRequestEvent, bot: Bot, matcher: Matcher):
     """加群申请处理函数"""
+
+    # ob标准的GroupRequestEvent没有nickname字段，需要单独获取(实际icqq是有此字段的, 但shamrock没有)
+    user_info = await bot.get_stranger_info(user_id=event.user_id)
+
     msg: Message = Message(MessageSegment.text("有一个加群通知，管理员快去看看吧~\n"))
     msg += MessageSegment.image(f'https://q1.qlogo.cn/g?b=qq&s=0&nk={event.user_id}')
-    msg += f'\nQQ号：{event.user_id}\n昵称：{event.dict()["nickname"]}\n{event.comment}'
+    msg += f'\nQQ号：{event.user_id}\n昵称：{user_info["nickname"]}\n{event.comment}'
 
     await matcher.send(msg)
