7d260d6ddddb05b1ed06fc1f6da6457dc7ad5eeb
Misaka-Mikoto <easy66@live.com>
2023-12-29 16:07:05 +0800

    * 修正类型提示错误

--- haruka_bot/plugins/group_request_notice.py
+++ haruka_bot/plugins/group_request_notice.py
@@ -11,7 +11,7 @@ group_request = on_request(priority=5)
 @group_request.handle()
 async def _(event: GroupRequestEvent, bot: Bot, matcher: Matcher):
     """加群申请处理函数"""
-    msg: Message = MessageSegment.text("有一个加群通知，管理员快去看看吧~\n")
+    msg: Message = Message(MessageSegment.text("有一个加群通知，管理员快去看看吧~\n"))
     msg += MessageSegment.image(f'https://q1.qlogo.cn/g?b=qq&s=0&nk={event.user_id}')
     msg += f'\nQQ号：{event.user_id}\n昵称：{event.dict()["nickname"]}\n{event.comment}'
 
