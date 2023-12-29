015c222595a0cbc6a3ed02c1bd629e60d073adc9
Misaka Mikoto <easy66@live.com>
2023-12-29 15:56:08 +0800

    * 修正加群请求的逻辑

--- haruka_bot/plugins/group_request_notice.py
+++ haruka_bot/plugins/group_request_notice.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from nonebot import on_request
 from nonebot.matcher import Matcher
-from nonebot.adapters.onebot.v11 import Bot
+from nonebot.adapters.onebot.v11 import Bot, MessageSegment, Message
 from nonebot.adapters.onebot.v11 import GroupRequestEvent
 from nonebot.log import logger
 
@@ -12,10 +12,10 @@ group_request = on_request(priority=5)
 async def _(event: GroupRequestEvent, bot: Bot, matcher: Matcher):
     """加群申请处理函数"""
     request_msg = f"bot:{bot.self_id}: {event.user_id} 申请加群 {event.group_id}, 加群信息:{event}"
-    user_info = await bot.get_stranger_info(user_id=event.user_id)
-    
     logger.info(request_msg)
-    logger.info(user_info)
 
-    await matcher.send(f"有人申请加群，申请信息：\n{request_msg}")
-    await matcher.send(f"申请人信息：\n{user_info}")
+    msg: Message = MessageSegment.text("有一个加群通知，管理员快去看看吧~\n")
+    msg += MessageSegment.image(f'https://q1.qlogo.cn/g?b=qq&s=0&nk={event.user_id}')
+    msg += f'\nQQ号：{event.user_id}\n昵称：{request_msg["nickname"]}\n{event.comment}'
+
+    await matcher.send(msg)
